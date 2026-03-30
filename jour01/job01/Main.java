d5eb9c44309ef8075aae8a4383411ab341aebd9d
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-30 11:48:41 +0200

    feat : complete job 01 and job 02

---
+++ jour01/job01/Main.java
@@ -0,0 +1,20 @@
+package jour01.job01;
+import java.util.Scanner; 
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Initialize the scanner object to get keyboard input
+        Scanner myFirstScanner = new Scanner (System.in);
+
+        System.out.println("Entrez votre texte à répeter :");
+        
+        // Use the scanner to get input with nextLine method
+        String myFirstInput = myFirstScanner.nextLine(); 
+        
+        System.out.println("Vous avez tapé : " + myFirstInput);
+
+    }
+
+}
---
+++ jour01/job02/Main.java
@@ -0,0 +1,21 @@
+package jour01.job02;
+import java.util.Scanner; 
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Initialize the scanner object to get keyboard input
+        Scanner myFirstScanner = new Scanner (System.in);
+
+        System.out.println("Quel est votre prénom? ");
+        
+        // Use the scanner to get input with nextLine method
+        String myFirstInput = myFirstScanner.nextLine(); 
+        
+        System.out.println("Bonjour " + myFirstInput);
+
+    }
+
+}
+

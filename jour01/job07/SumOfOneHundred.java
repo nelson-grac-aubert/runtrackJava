1ea2dac0b7c365fbe255a71e837a7548b0f1826f
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-30 14:22:06 +0200

    feat : complete job 07, start job 08

---
+++ jour01/job07/SumOfOneHundred.java
@@ -0,0 +1,23 @@
+package jour01.job07;
+
+public class SumOfOneHundred {
+
+    public static int CalculateTheSum(String[] args) { 
+
+        int sum = 0; 
+
+        for(int i = 1; i <= 100; i++) {
+
+            sum += i;
+        }
+
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("La somme des entiers de 0 à 100 est : " + CalculateTheSum(args));
+        
+    }
+}

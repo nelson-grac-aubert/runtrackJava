d723fd418d2155433420a708bde2e95e46eba8f5
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-31 10:09:37 +0200

    fix : instantiate in job 00

--- jour02/job00/Person.java
+++ jour02/job00/Person.java
@@ -19,8 +19,33 @@ public class Person {
     private String adress;
     private int phoneNumber; 
 
+    Person(String famName, String firName, LocalDate birth, String birPla, String adr, int num) {
+
+        this.familyName = famName;
+        this.firstName = firName; 
+        this.birthDate = birth;
+        this.birthPlace = birPla; 
+        this.adress = adr; 
+        this.phoneNumber = num; 
+
+    }
+
+    public static void main(String[] args) { 
+
+        LocalDate myDate = LocalDate.of(1996, 1, 16);
+
+        Person leGrosNelson = new Person("Grac", "Nelson", 
+                            myDate, "Hyères", "Ma grosse adresse", 666);
+
+        System.out.println(leGrosNelson);
+
+    }
+
     /* Les attributs publics sont accessibles "partout", même en dehors de la classe
-    Les attributs protected et private ont besoin d'accesseurs
+    Les attributs protected sont accessibles dans la classe et ses enfants
+    Les attributs private sont accessibles uniquement dans la classe
+    Les attributs protected ont besoin d'accesseurs en dehors de la classe et de ses enfants
+    Les attributs private ont besoun d'accesseurs en dehors de la classe
     */
 
 }

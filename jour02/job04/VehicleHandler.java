f9e1f72900230801fab07f669cb59ef43bee8c12
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-31 14:25:30 +0200

    feat : complete exercice 4.2

--- jour02/job04/VehicleHandler.java
+++ jour02/job04/VehicleHandler.java
@@ -8,13 +8,16 @@ public class VehicleHandler {
 
         String brand; 
         LocalDate buyDate; 
-        long buyPrice;
-        long currentPrice; 
+        float buyPrice;
+        double currentPrice; 
 
         Vehicle(String brand, LocalDate buyDate, long buyPrice) {
+
             this.brand = brand;
             this.buyDate = buyDate; 
             this.buyPrice = buyPrice; 
+
+            determineCurrentPrice(2026);
 
         }
 
@@ -24,7 +27,7 @@ public class VehicleHandler {
         public LocalDate getBuyDate() {
             return this.buyDate;
         }
-        public long getBuyPrice() {
+        public float getBuyPrice() {
             return this.buyPrice;
         }
 
@@ -32,6 +35,21 @@ public class VehicleHandler {
             System.out.println(String.format("Un véhicule de marque %s, acheté le %s pour %s euros", 
                                this.getBrand(), this.getBuyDate(), this.getBuyPrice()));
         }
+
+    public double determineCurrentPrice(int currentYear) { 
+
+        long ageOfTheVehicle = currentYear - this.getBuyDate().getYear();
+
+        double newPrice = this.buyPrice * Math.pow(0.99, ageOfTheVehicle);
+        if (newPrice < 0) {
+            newPrice = 0;
+        }
+
+        this.currentPrice = newPrice;
+
+        return this.currentPrice;
+
+    }
    
     }
 
@@ -58,15 +76,52 @@ public class VehicleHandler {
 
         }
 
+        public double determineCurrentPrice(int currentYear) { 
+            
+            // - 2% every year
+            long ageOfTheVehicle = currentYear - this.getBuyDate().getYear();
+
+            double newPrice = this.buyPrice * Math.pow(0.98, ageOfTheVehicle);
+
+            // -5% every 10 000km 
+
+            int amountOf10K = Math.round(this.distance / 10000); 
+
+            newPrice = newPrice * Math.pow(0.95, amountOf10K);
+
+            // -10% if Fiat or Peugeot
+
+            if(this.getBrand() == "Peugeot" || this.getBrand() == "Fiat") {
+                newPrice *= 0.9;
+            }
+
+            // +20% if Porsche or Ferrarri 
+
+            if(this.getBrand() == "Porsche" || this.getBrand() == "Ferrari") {
+                newPrice *= 1.2;
+            }
+
+            // If negative => 0 
+
+            if (newPrice < 0) {
+                newPrice = 0;
+            }
+
+            this.currentPrice = newPrice; 
+            return newPrice;
+        }
+
     }
 
     public static class Plane extends Vehicle { 
 
         String motorType; 
+        int flightHours;
 
-        Plane(String brand, LocalDate buyDate, long buyPrice, String motorType) { 
+        Plane(String brand, LocalDate buyDate, long buyPrice, String motorType, int flightHours) { 
             super(brand, buyDate, buyPrice); 
             this.motorType = motorType; 
+            this.flightHours = flightHours;
         }
 
         public void show() { 
@@ -75,18 +130,38 @@ public class VehicleHandler {
             System.out.println(String.format("Plus précisément, un avion a moteur de type %s", this.motorType));
 
         }
+
+        public double determineCurrentPrice(int currentYear) { 
+
+            int amountOfSlices;
+
+            if (this.motorType == "hélice") { 
+                amountOfSlices = Math.round(this.flightHours / 100);
+            }
+            else { 
+                amountOfSlices = Math.round(this.flightHours / 1000);
+            }
+
+            double newPrice = this.buyPrice * Math.pow(0.9, amountOfSlices);
+
+            this.currentPrice = newPrice;
+            return newPrice;
+        }
     }
     
     public static void debugVehicle() { 
 
-        Vehicle myVehicle = new Vehicle("TMAX", LocalDate.of(1996, 1, 16), 14000);
+        Vehicle myVehicle = new Vehicle("TMAX", LocalDate.of(2024, 1, 16), 14000);
         myVehicle.show();
+        System.out.println(myVehicle.currentPrice);
 
-        Car myCar = new Car("Toyota", LocalDate.of(2000, 1,1), 25000, 500, (byte) 5, 500, 127000);
+        Car myCar = new Car("Fiat", LocalDate.of(2000, 1,1), 25000, 500, (byte) 5, 500, 127000);
         myCar.show();
+        System.out.println(myCar.currentPrice);
 
-        Plane myPlane = new Plane("Airbus", LocalDate.of(1995, 2, 2), 2000000, "Reaction thermonucléaire sa mère");
+        Plane myPlane = new Plane("Airbus", LocalDate.of(1995, 2, 2), 2000000, "Reaction thermonucléaire sa mère", 260);
         myPlane.show();
+        System.out.println(myPlane.currentPrice);
 
     }
 

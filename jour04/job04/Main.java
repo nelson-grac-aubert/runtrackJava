2e5ff637eab3cbc4f083812a3344c63c2e622447
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-02 15:16:41 +0200

    feat : complete exercice 5

--- jour04/job04/Main.java
+++ jour04/job04/Main.java
@@ -2,6 +2,31 @@ package jour04.job04;
 
 public class Main {
 
-    
+    private static String formatTheElapsedTime(long start, long stop) { 
+
+        long milisecondsDelta = stop - start;
+
+        return String.format("The program took %s miliseconds to run", milisecondsDelta);
+
+    }
+    public static void main(String[] args) {
+
+        final int maxCount = User.getUserInput();
+
+        // Timestamp the start time
+        final long start = System.currentTimeMillis();
+
+        for(int i = 1; i <= maxCount - 1; i++) {
+            System.out.println("I'm counting... " + i);
+        }
+
+        System.out.println("I counted to " + maxCount + "!");
+
+        // Timestamp the stop time, print elapsed
+        final long stop = System.currentTimeMillis();
+        System.out.println(formatTheElapsedTime(start, stop));
+
+
+    }
 
 }   

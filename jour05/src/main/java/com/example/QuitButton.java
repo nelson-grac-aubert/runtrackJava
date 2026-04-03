d3ead4429b607b2afaba0b45e03d6c27d368073e
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-03 15:01:28 +0200

    docs : add comments to QuitButton class

--- jour05/src/main/java/com/example/QuitButton.java
+++ jour05/src/main/java/com/example/QuitButton.java
@@ -6,9 +6,10 @@ import javafx.scene.control.Button;
 public class QuitButton extends Button {
     
     QuitButton(String text) {
-
+        // Use the constructor of the Button class to set a text
         super(text);
         
+        // Method to set the action of a button
         this.setOnAction(e -> Platform.exit());
     }
 }
